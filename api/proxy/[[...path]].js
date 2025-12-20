c61b23ba9200699a7b4800d667c2725ef1806687
Firdavs Iskandarov <iskandarovfirdavs09@gmail.com>
2025-12-20 19:08:35 +0500

    5th try

--- api/proxy/[[...path]].js
+++ api/proxy/[[...path]].js
@@ -5,6 +5,22 @@ export default async function handler(req, res) {
   const endpoint = "/" + path.join("/"); // /users/login
   const url = `https://api.e-kundalikfu.uz${endpoint}`;
 
+  let body;
+  if (req.method !== "GET") {
+    try {
+      body = await new Promise((resolve, reject) => {
+        let data = "";
+        req.on("data", (chunk) => {
+          data += chunk;
+        });
+        req.on("end", () => resolve(data));
+        req.on("error", (err) => reject(err));
+      });
+    } catch (err) {
+      return res.status(400).json({ error: "Body parse error" });
+    }
+  }
+
   try {
     const response = await fetch(url, {
       method: req.method,
@@ -12,7 +28,7 @@ export default async function handler(req, res) {
         "Content-Type": req.headers["content-type"] || "application/json",
         Authorization: req.headers["authorization"] || "",
       },
-      body: req.method !== "GET" ? req.body : undefined,
+      body: req.method !== "GET" ? body : undefined,
     });
 
     const data = await response.json();
